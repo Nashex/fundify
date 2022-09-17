1d649c890bcc43b0ec597ecb441ec604e6190a2d
Jerome <jfrancis.jh@gmail.com>
2022-09-17 19:53:19 -0400

    Removed carousel, started payment button

--- code/website/components/widget/DonationTier.tsx
+++ code/website/components/widget/DonationTier.tsx
@@ -12,7 +12,7 @@ export default function ({ title, description, amount }: Props) {
                 <div className="flex flex-col w-full">
                     <h1 className="py-1 px-2 text-green-500 text-3xl font-bold">{title}</h1>
                     <p className="text-black text-md leading-none px-2 truncate-hidden">{description}</p>
-                    <div className="mx-auto justify-center align-middle py-5">
+                    <div className="mx-auto justify-center align-middle py-5 mt-auto">
                         <p className="text-black text-xl font-bold">${amount}</p>
                     </div>
                 </div>
