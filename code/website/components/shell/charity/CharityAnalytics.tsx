95595d410b0347857977dc30a3fbdebb80726adb
Jerome <jfrancis.jh@gmail.com>
2022-09-17 23:23:07 -0400

    Started analytics

---
+++ code/website/components/shell/charity/CharityAnalytics.tsx
@@ -0,0 +1,40 @@
+import React, { ReactElement } from 'react'
+import { Charity } from '../../../types/types'
+import TierDashboardCard from '../../donate/TierDashboardCard'
+
+interface Props {
+    charity: Charity
+}
+
+export default function CharityAnalytics({ charity }: Props) {
+    return (
+        <div className="mb-6">
+            <h1 className="text-2xl font-medium">{charity.name}</h1>
+            <p className="text-xl text-gray-400">{charity.desc}</p>
+            <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 my-2 space-x-4">
+                <TierDashboardCard
+                    charity={charity}
+                    name="Patron"
+                    desc="Puts 5 more trees in the ground"
+                    amount={5}
+                    type="one-time"
+                    className="blur-[4px] opacity-70 mb-4"
+                    create
+                />
+                {
+                    charity.tiers?.map((o, i) => {
+                        return (<TierDashboardCard
+                            id={o.id}
+                            charity={charity}
+                            name={o.name}
+                            desc={o.desc}
+                            amount={o.amount}
+                            type={o.type}
+                            className="mb-4"
+                        />)
+                    })
+                }
+            </div>
+        </div>
+    )
+}
