2c795a1a081109cae05f7ac6b26ac1dd81af17cf
Jerome <jfrancis.jh@gmail.com>
2022-09-17 20:52:45 -0400

    Modal done.

--- code/website/components/widget/DonationTier.tsx
+++ code/website/components/widget/DonationTier.tsx
@@ -1,21 +1,32 @@
 import React from 'react'
+import PaymentButton from './PaymentButton';
 
 type Props = {
     title: string,
     description: string,
-    amount: string
+    amount: string,
+    recurring: boolean
 }
 
-export default function ({ title, description, amount }: Props) {
+export default function ({ title, description, amount, recurring }: Props) {
     return (
-            <div className="flex rounded-lg w-full bg-red-200">
-                <div className="flex flex-col w-full">
-                    <h1 className="py-1 px-2 text-green-500 text-3xl font-bold">{title}</h1>
-                    <p className="text-black text-md leading-none px-2 truncate-hidden">{description}</p>
-                    <div className="mx-auto justify-center align-middle py-5 mt-auto">
-                        <p className="text-black text-xl font-bold">${amount}</p>
+        <div className="flex rounded-lg w-full border-[.5px] border-slate-400 mx-2">
+            <div className="flex flex-col w-full max-w-sm">
+                <h1 className="pt-4 px-4 text-green-400 text-3xl font-bold">{title}</h1>
+                <p className="text-gray-600 text-md leading-none px-4 truncate-hidden">{description}</p>
+                <div className="mx-auto justify-center align-middle py-5 mt-auto">
+                    <div className="flex flex-row items-end">
+                        <p className="text-green-400 text-3xl font-bold">${amount}</p>
+                        {
+                            recurring ?
+                                <p className="text-gray-400 text-md">&nbsp;monthly.</p>
+                                :
+                                <p className="text-gray-400 text-md">&nbsp;once.</p>
+                        }
                     </div>
                 </div>
+                <PaymentButton />
             </div>
+        </div>
     )
 }
