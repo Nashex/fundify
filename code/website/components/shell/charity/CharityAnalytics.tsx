873a94668e7eea519503de96a3c551d675e7ada5
Jerome <jfrancis.jh@gmail.com>
2022-09-18 00:14:39 -0400

    Stubbed linear over time analytics

--- code/website/components/shell/charity/CharityAnalytics.tsx
+++ code/website/components/shell/charity/CharityAnalytics.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react'
 import { Charity } from '../../../types/types'
 import TierDashboardCard from '../../donate/TierDashboardCard'
 import DonationsReceived from '../../analytics/DonationsReceived';
+import LinearOverTime from '../../analytics/LinearOverTime';
 
 interface Props {
     charity: Charity
@@ -11,8 +12,13 @@ export default function CharityAnalytics({ charity }: Props) {
     return (
         <div className="mb-6">
             <h1 className="text-2xl font-medium">{charity.name}</h1>
-            <div className = "flex flex-row">
-                <DonationsReceived charity={charity} time='all-time' average = {true}/>
+            <div className="flex flex-col">
+                <div className="flex flex-row my-1">
+                    <DonationsReceived charity={charity} time='all-time' average={true} />
+                </div>
+                <div className = "flex flex-row my-1">
+                    <LinearOverTime charity = {charity} title = 'Donations Over Time' y = 'donation' average = {false}/>
+                </div>
             </div>
         </div>
     )
