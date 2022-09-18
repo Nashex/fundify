34676dfa03e0b65d00051ab3c1eb0b09a7c940f6
Josh N <45410780+Nashex@users.noreply.github.com>
2022-09-18 05:33:30 -0400

    Cors issues

--- code/website/next.config.js
+++ code/website/next.config.js
@@ -4,7 +4,15 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ["*"]
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
